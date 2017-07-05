08d1b3ba31abfa41a93538d91f906aebabddab6a
softwaredavid@sina.com <softwaredavid@sina.com>
2017-07-05 08:42:52 +0800

    修改路径

---
+++ CodeProject/CodeProject/Utilty/PayManager.swift
@@ -0,0 +1,13 @@
+//
+//  PayManager.swift
+//  CodeProject
+//
+//  Created by Apple on 2017/7/4.
+//  Copyright © 2017年 WangDaoLeTu. All rights reserved.
+//
+
+import UIKit
+
+class PayManager: NSObject {
+
+}
